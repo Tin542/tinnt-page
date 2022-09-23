a5f8aacf11a02573e223a7e4b9c7d24ca157cd5b
uikui <tinnt@gmail.com>
2022-09-24 01:18:37 +0700

    first commit

---
+++ src/portfolio.js
@@ -0,0 +1,389 @@
+/* Change this file to get your personal Porfolio */
+
+// Website related settings
+const settings = {
+  isSplash: false, // Change this to false if you don't want Splash screen.
+};
+
+//SEO Related settings
+const seo = {
+  title: "Nguyễn Thành Tín",
+  description: "Front-End Developer",
+  og: {
+    title: "Tín Portfolio",
+    type: "website",
+    url: "",
+  },
+};
+
+//Home Page
+const greeting = {
+  title: "Nguyễn Thành Tín",
+  logo_name: "Nguyễn Thành Tín",
+  nickname: "Robin",
+  subTitle: "Front-End Developer",
+
+  portfolio_repository: "https://github.com/Tin542",
+  githubProfile: "https://github.com/Tin542",
+};
+
+const socialMediaLinks = [
+  /* Your Social Media Link */
+  // github: "https://github.com/ashutosh1919",
+  // linkedin: "https://www.linkedin.com/in/ashutosh-hathidara-88710b138/",
+  // gmail: "ashutoshhathidara98@gmail.com",
+  // gitlab: "https://gitlab.com/ashutoshhathidara98",
+  // facebook: "https://www.facebook.com/laymanbrother.19/",
+  // twitter: "https://twitter.com/ashutosh_1919",
+  // instagram: "https://www.instagram.com/layman_brother/"
+
+  {
+    name: "Github",
+    link: "https://github.com/Tin542",
+    fontAwesomeIcon: "fa-github", // Reference https://fontawesome.com/icons/github?style=brands
+    backgroundColor: "#181717", // Reference https://simpleicons.org/?q=github
+  },
+  {
+    name: "Skype",
+    link: "https://join.skype.com/invite/xkpT0z03H1qE",
+    fontAwesomeIcon: "fa-skype", // Reference https://fontawesome.com/icons/instagram?style=brands
+    backgroundColor: "#00AFF0", // Reference https://simpleicons.org/?q=instagram
+  },
+  {
+    name: "Facebook",
+    link: "https://www.facebook.com/thanhtin.nguyen.96930/",
+    fontAwesomeIcon: "fa-facebook", // Reference https://fontawesome.com/icons/instagram?style=brands
+    backgroundColor: "Blue", // Reference https://simpleicons.org/?q=instagram
+  },
+  {
+    name: "Instagram",
+    link: "https://www.instagram.com/ntt_0504/",
+    fontAwesomeIcon: "fa-instagram", // Reference https://fontawesome.com/icons/instagram?style=brands
+    backgroundColor: "#E4405F", // Reference https://simpleicons.org/?q=instagram
+  },
+  {
+    name: "Youtube",
+    link: "https://www.youtube.com/channel/UCoI-iwd8sYWtPdLRY87QXQA/videos",
+    fontAwesomeIcon: "fa-youtube", // Reference https://fontawesome.com/icons/instagram?style=brands
+    backgroundColor: "#FF0000", // Reference https://simpleicons.org/?q=instagram
+  },
+];
+
+const skills = {
+  data: [
+    {
+      title: "Personality",
+      fileName: "DataScienceImg",
+      skills: [
+        "⚡ Hardworking.",
+        "⚡ Enjoy learning more new knowledge.",
+        "⚡ Introver but want to make new friends =(^.^)=",
+        "⚡ Dog lover.",
+      ],
+      
+    },
+    {
+      title: "Hobby",
+      fileName: "FullStackImg",
+      skills: [
+        "⚡ Play game",
+        "⚡ Watching movie",
+        "⚡ Walking",
+        "⚡ Listen chill music",
+      ],
+      
+    },
+  
+  ],
+};
+
+// Education Page
+const competitiveSites = {
+  competitiveSites: [
+    {
+      siteName: "HackerRank",
+      iconifyClassname: "simple-icons:hackerrank",
+      style: {
+        color: "#2EC866",
+      },
+      profileLink: "https://www.hackerrank.com/layman_brother",
+    },
+    {
+      siteName: "Codechef",
+      iconifyClassname: "simple-icons:codechef",
+      style: {
+        color: "#5B4638",
+      },
+      profileLink: "https://www.codechef.com/users/albert_1919",
+    },
+    {
+      siteName: "Codeforces",
+      iconifyClassname: "simple-icons:codeforces",
+      style: {
+        color: "#1F8ACB",
+      },
+      profileLink: "http://codeforces.com/profile/layman_brother",
+    },
+    {
+      siteName: "Hackerearth",
+      iconifyClassname: "simple-icons:hackerearth",
+      style: {
+        color: "#323754",
+      },
+      profileLink: "https://www.hackerearth.com/@albert391",
+    },
+    {
+      siteName: "Kaggle",
+      iconifyClassname: "simple-icons:kaggle",
+      style: {
+        color: "#20BEFF",
+      },
+      profileLink: "https://www.kaggle.com/laymanbrother",
+    },
+  ],
+};
+
+const degrees = {
+  degrees: [
+    {
+      title: "Indian Institute of Information Technology Kurnool",
+      subtitle: "B.Tech. in Computer Engineering",
+      logo_path: "iiitk_logo.png",
+      alt_name: "IIITDM Kurnool",
+      duration: "2016 - Present",
+      descriptions: [
+        "⚡ I have studied basic software engineering subjects like DS, Algorithms, DBMS, OS, CA, AI etc.",
+        "⚡ Apart from this, I have done courses on Deep Learning, Data Science, Cloud Computing and Full Stack Development.",
+        "⚡ I was selected for Merit cum Means Scholarship which is given to top 10% of students in college. I have received award from respected director for consistently best performance in academics.",
+      ],
+      website_link: "http://iiitk.ac.in",
+    },
+  ],
+};
+
+const certifications = {
+  certifications: [
+    {
+      title: "Machine Learning",
+      subtitle: "- Andrew Ng",
+      logo_path: "stanford_logo.png",
+      certificate_link:
+        "https://www.coursera.org/account/accomplishments/verify/22MTSSC5WDTM",
+      alt_name: "Stanford University",
+      color_code: "#8C151599",
+    },
+    {
+      title: "Deep Learning",
+      subtitle: "- Andrew Ng",
+      logo_path: "deeplearning_ai_logo.png",
+      certificate_link:
+        "https://www.coursera.org/account/accomplishments/specialization/H8CPSFXAJD2G",
+      alt_name: "deeplearning.ai",
+      color_code: "#00000099",
+    },
+    {
+      title: "ML on GCP",
+      subtitle: "- GCP Training",
+      logo_path: "google_logo.png",
+      certificate_link:
+        "https://www.coursera.org/account/accomplishments/specialization/EB4VJARK8647",
+      alt_name: "Google",
+      color_code: "#0C9D5899",
+    },
+    {
+      title: "Data Science",
+      subtitle: "- Alex Aklson",
+      logo_path: "ibm_logo.png",
+      certificate_link:
+        "https://www.coursera.org/account/accomplishments/specialization/PLEAPCSJBZT5",
+      alt_name: "IBM",
+      color_code: "#1F70C199",
+    },
+    {
+      title: "Big Data",
+      subtitle: "- Kim Akers",
+      logo_path: "microsoft_logo.png",
+      certificate_link:
+        "https://drive.google.com/file/d/164zKCFOsI4vGqokc-Qj-e_D00kLDHIrG/view",
+      alt_name: "Microsoft",
+      color_code: "#D83B0199",
+    },
+    {
+      title: "Advanced Data Science",
+      subtitle: "- Romeo Kienzler",
+      logo_path: "ibm_logo.png",
+      certificate_link:
+        "https://www.coursera.org/account/accomplishments/verify/BH2T9BRU87BH",
+      alt_name: "IBM",
+      color_code: "#1F70C199",
+    },
+    {
+      title: "Advanced ML on GCP",
+      subtitle: "- GCP Training",
+      logo_path: "google_logo.png",
+      certificate_link:
+        "https://www.coursera.org/account/accomplishments/verify/5JZZM7TNQ2AV",
+      alt_name: "Google",
+      color_code: "#0C9D5899",
+    },
+    {
+      title: "DL on Tensorflow",
+      subtitle: "- Laurence Moroney",
+      logo_path: "deeplearning_ai_logo.png",
+      certificate_link:
+        "https://www.coursera.org/account/accomplishments/verify/6T4DCUGNK8J8",
+      alt_name: "deeplearning.ai",
+      color_code: "#00000099",
+    },
+    {
+      title: "Fullstack Development",
+      subtitle: "- Jogesh Muppala",
+      logo_path: "coursera_logo.png",
+      certificate_link:
+        "https://www.coursera.org/account/accomplishments/certificate/NRANJA66Y2YA",
+      alt_name: "Coursera",
+      color_code: "#2A73CC",
+    },
+    {
+      title: "Kuberenetes on GCP",
+      subtitle: "- Qwiklabs",
+      logo_path: "gcp_logo.png",
+      certificate_link:
+        "https://google.qwiklabs.com/public_profiles/e4d5a92b-faf6-4679-a70b-a9047c0cd750",
+      alt_name: "GCP",
+      color_code: "#4285F499",
+    },
+    {
+      title: "Cryptography",
+      subtitle: "- Saurabh Mukhopadhyay",
+      logo_path: "nptel_logo.png",
+      certificate_link:
+        "https://drive.google.com/open?id=1z5ExD_QJVdU0slLkp8CBqSF3-C3g-ro_",
+      alt_name: "NPTEL",
+      color_code: "#FFBB0099",
+    },
+    {
+      title: "Cloud Architecture",
+      subtitle: "- Qwiklabs",
+      logo_path: "gcp_logo.png",
+      certificate_link:
+        "https://google.qwiklabs.com/public_profiles/5fab4b2d-be6f-408c-8dcb-6d3b58ecb4a2",
+      alt_name: "GCP",
+      color_code: "#4285F499",
+    },
+  ],
+};
+
+// Experience Page
+const experience = {
+  title: "Experience",
+  subtitle: "Work, Internship and Volunteership",
+  description:
+    "",
+  header_image_path: "experience.svg",
+  sections: [
+    {
+      title: "Intership",
+      experiences: [
+        {
+          title: "Rookie program at NashTech",
+          company: "Nash Tech",
+          company_url: "https://nashtechglobal.com/",
+          logo_path: "nash_logo.png",
+          duration: "May 2021 - September 2021",
+          location: "",
+          description:
+            "I joined Nash tech's Rookie program to complete my internship. At nash tech, I was trained in ReactJS, Java springboot, and the soft skills needed for 1 programmer. I was also involved in the company's asset management project.",
+          color: "#0879bf",
+        },
+      ],
+    },
+    {
+      title: "Work",
+      experiences: [
+        // {
+        //   title: "Data Science Research Intern",
+        //   company: "Delhivery Pvt. Ltd.",
+        //   company_url: "https://www.delhivery.com/",
+        //   logo_path: "delhivery_logo.png",
+        //   duration: "May 2019 - Sept 2019",
+        //   location: "Gurgaon, Haryana",
+        //   description:
+        //     "I have worked on project of predicting freight rates based on previous data. There were two objectives: (1) To build a forecasting engine to predict daily freight rates. (2) To embed feature in the model which can explain the seasonal major changes in freight rate based on regions and locations. I have closely worked with deep learning models in combination with statistical methods to create solution for this. At the end of internship, I had created model deployed on AWS EC2 with the use of Kafka stream jobs, ElasticSearch and PostgreSQL.",
+        //   color: "#ee3c26",
+        // },
+      ],
+    },
+    {
+      title: "Volunteerships",
+      experiences: [
+        // {
+        //   title: "Google Explore ML Facilitator",
+        //   company: "Google",
+        //   company_url: "https://about.google/",
+        //   logo_path: "google_logo.png",
+        //   duration: "June 2019 - April 2020",
+        //   location: "Hyderabad, Telangana",
+        //   description:
+        //     "Explore Machine Learning (ML) is a Google-sponsored program for university students to get started with Machine Learning. The curriculum offers 3 tracks of ML Content (Beginner, Intermediate, Advanced) and relies on university student facilitators to train other students on campus and to build opensource projects under this program.",
+        //   color: "#4285F4",
+        // },
+        
+      ],
+    },
+  ],
+};
+
+// Projects Page
+const projectsHeader = {
+  title: "Projects",
+  description:
+    "",
+  avatar_image_path: "projects_image.svg",
+};
+
+// Contact Page
+const contactPageData = {
+  contactSection: {
+    title: "Contact Me",
+    profile_image_path: "Tin.png",
+    description:
+      "I am available on almost every social media. You can message me, I will reply within 24 hours. I can help you with React, Java.",
+  },
+  blogSection: {
+    title: "Blogs",
+    subtitle:
+      "For individual fundamental empowerment, I like to write powerful lessons that create impact on each of the reader individually to change the core of their character.",
+    link: "https://alberthathidara.wordpress.com",
+    avatar_image_path: "blogs_image.svg",
+  },
+  addressSection: {
+    title: "Address",
+    subtitle: "62L Đường 32B Phường 10 Quận 6, TP HCM",
+    avatar_image_path: "address_image.svg",
+    location_map_link:
+      "https://www.google.com/maps/d/viewer?mid=1iOuLbw9_GB6ZBs8pg61OnElHzQE&hl=en_US",
+  },
+  phoneSection: {
+    title: "Phone Number",
+    subtitle: "0773114946",
+  },
+  emailSection : {
+    title: "Email",
+    subtitle: "tinnt0504@gmail.com"
+  }
+};
+
+export {
+  settings,
+  seo,
+  greeting,
+  socialMediaLinks,
+  skills,
+  competitiveSites,
+  degrees,
+  certifications,
+  experience,
+  projectsHeader,
+  contactPageData,
+};
